34e9c085e32f449456e85d02ae67a770c7910aac
Lucas Neves <lucas.ccdr@gmail.com>
2025-02-05 15:29:20 -0300

    Incluido scanner para interação com o usuario.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
+/** Objetivo - Criar um projeto que conta as palavras de uma frase
+ 1 - Obter uma frase do usuario
+ 2 - Contar as palavras da frase e exibir o resultado
+ 3 - Solicitar se o usuario quer digitar outra frase
+ 4 - Se sim, repetir a lógica 1 e 2
+ 5 - Se não, encerrar o programa
+ */
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite a frase que você quer contar as palavras: ");
+        String frase = leitor.nextLine();
+
     }
 }
